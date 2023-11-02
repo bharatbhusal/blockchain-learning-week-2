6602a4e2754323de4c035e5245e3bb563c013109
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-11-02 23:56:51 +0530

    basic contract done

---
+++ exercise10.sol
@@ -0,0 +1,22 @@
+// Exercise 10: Basic Smart Contract
+//   - Write a smart contract named **`SimpleStorage`** that stores a number and allows functions to get and set that number.
+//   - Deploy the contract using the Remix Ethereum IDE.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract SimpleStorage {
+    uint256 balance;
+
+    constructor(uint256 num) {
+        balance = num;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    function setBalance(uint256 num) public {
+        balance = num;
+    }
+}
