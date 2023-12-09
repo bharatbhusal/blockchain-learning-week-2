77f46c17f02bbd3c778298fcb846620f7bd96b14
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-09 21:28:38 +0530

    adding comments

--- Exercise10/contracts/SimpleStorage.sol
+++ Exercise10/contracts/SimpleStorage.sol
@@ -5,18 +5,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// SimpleStorage contract to store and retrieve a uint256 balance
 contract SimpleStorage {
+    // State variable to store the balance
     uint256 balance;
 
+    // Constructor to initialize the contract with an initial balance
     constructor(uint256 initialBalance) {
+        // Set the initial balance when the contract is deployed
         balance = initialBalance;
     }
 
+    // Function to retrieve the current balance
     function getBalance() public view returns (uint256) {
+        // Return the current balance without modifying the state
         return balance;
     }
 
+    // Function to set a new balance
     function setBalance(uint256 num) public {
+        // Update the balance with the provided value
         balance = num;
     }
 }
